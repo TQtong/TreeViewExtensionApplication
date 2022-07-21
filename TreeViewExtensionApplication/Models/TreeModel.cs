eb5a5c470697acd8375655130543e085726bfb30
TQ <tangqi@southzn.com>
2022-07-21 14:47:21 +0800

    添加项目文件。

---
+++ TreeViewExtensionApplication/Models/TreeModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeViewExtensionApplication.Models
+{
+    public class TreeModel : BindableObject
+    {
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<TreeModel> Children { get; set; } = new ObservableCollection<TreeModel>();
+    }
+}
---
+++ TreeViewExtensionApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using TreeViewExtensionApplication.Common;
+using TreeViewExtensionApplication.Models;
+
+namespace TreeViewExtensionApplication.ViewModels
+{
+    public class MainViewModel : BindableObject
+    {
+        public ObservableCollection<TreeModel> TreeModels { get; set; } = new ObservableCollection<TreeModel>();
+
+        private TreeModel selectItem;
+
+        public TreeModel SelectItem
+        {
+            get => selectItem;
+            set
+            {
+                selectItem = value;
+                Console.WriteLine(SelectItem);
+                OnPropertyChanged();
+            }
+        }
+
+        public DelegateCommand<TreeView> Selected { get; private set; }
+
+        public MainViewModel()
+        {
+            Selected = new DelegateCommand<TreeView>(Tree_SelectedItemChanged);
+            CreateTree();
+        }
+
+        private void CreateTree()
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                TreeModel treeModel = new TreeModel()
+                {
+                    Name = $"我是{i}"
+                };
+
+                TreeModels.Add(treeModel);
+
+                for (int j = 0; j < 10; j++)
+                {
+                    treeModel.Children.Add(new TreeModel()
+                    {
+                        Name = $"我是{i}的{j}个孩子"
+                    });
+                }
+            }
+        }
+
+        public void Tree_SelectedItemChanged(TreeView view)
+        {
+            SelectItem = view.SelectedItem as TreeModel;
+        }
+    }
+}
